dedd70599d69d43d9bc826fde33bbd620c6173dd
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-01-19 04:24:58 +0200

    add task 1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const categoriesList = document.querySelector('ul#categories');
+
+const categoriesItems = categoriesList.querySelectorAll('li.item');
+
+
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach((item) => {
+  const headingText = item.querySelector("h2").textContent;
+  const liItems = item.querySelectorAll("ul>li");
+  console.log(`Category: ${headingText}`);
+  console.log(`Elements: ${liItems.length}`);
+});
